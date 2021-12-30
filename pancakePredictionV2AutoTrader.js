58e74c564a625e510f093b8caa278785eaf1fd3c
zhihuizhilv <zhihuizhilv@foxmail.com>
2021-12-30 22:02:27 +0800

    add

--- pancakePredictionV2AutoTrader.js
+++ pancakePredictionV2AutoTrader.js
@@ -17,16 +17,36 @@ async function loadPancakePredictionV2() {
     pancakePrediction = new web3.eth.Contract(ABIS.PancakePredictionV2, "0x18b2a687610328590bc8f2e5fedde3b582a49cda");
 }
 
+async function sleepTimer(timeout) {
+    await new Promise((resolve, reject) => {
+        setTimeout(resolve, timeout);
+    });
+}
+
+
 let main = async() => {
     await initWallet();
     await loadPancakePredictionV2();
 
     // currentEpoch
     let currentEpoch = await pancakePrediction.methods.currentEpoch().call();
+    console.log("currentEpoch:", currentEpoch);
+
 
     // 下注下跌
     let betRet = await pancakePrediction.methods.betBear(currentEpoch).send({from:provider.addresses[0], value:web3.utils.toWei('0.1', 'ether')});
     console.log(betRet);
+
+    // 等待开奖
+    for(;;) {
+        let round = await pancakePrediction.methods.rounds(currentEpoch).call();
+        if (round.oracleCalled) {
+            break;
+        } else {
+            await sleepTimer(1000*1);
+            continue;
+        }
+    }
 
     // 下注上涨
     // await pancakePrediction.methods.betBull(currentEpoch).send({value:web3.utils.toWei('0.1', 'ether')});
